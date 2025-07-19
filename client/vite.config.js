2a59caa145a92eb6078894830c65fb3e3684419a
Your Name <ronijames008@gmail.com>
2025-07-19 17:54:13 +0530

    Express server, Vite Proxy setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/proxy': {
+        target: 'http://localhost:8082', // Backend server
+        changeOrigin: true, // Ensures the Host header matches the target
+        rewrite: (path) => path.replace(/^\/proxy/, ''), // Remove /proxy from the path
+      },
+    },
+  },
 })
