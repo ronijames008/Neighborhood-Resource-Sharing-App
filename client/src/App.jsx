6172174ff2a38d8679bca8fe07d5ba081397421e
Your Name <ronijames008@gmail.com>
2025-07-19 16:11:48 +0530

    react-router-dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,69 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            {
+              /* Your Home/Catalog Component */
+            }
+          }
+        />
+        <Route
+          path="/items/:id"
+          element={
+            {
+              /* Your Item Details Component */
+            }
+          }
+        />
+        <Route
+          path="/add-item"
+          element={
+            {
+              /* Your Add New Item Component */
+            }
+          }
+        />
+        <Route
+          path="/my-requests"
+          element={
+            {
+              /* Your My Requests Component (Bonus) */
+            }
+          }
+        />
+        <Route
+          path="/map"
+          element={
+            {
+              /* Your Map View Component (Bonus) */
+            }
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            {
+              /* Your User Profile Component (Bonus) */
+            }
+          }
+        />
+        <Route
+          path="*"
+          element={
+            {
+              /* Your 404 Not Found Component */
+            }
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
