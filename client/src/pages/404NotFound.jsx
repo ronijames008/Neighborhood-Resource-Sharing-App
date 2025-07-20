85ec43cb9a41a54a4076f40d5f72d3dcaa689e80
Your Name <ronijames008@gmail.com>
2025-07-20 18:03:58 +0530

    404NotFound setup

--- client/src/pages/404NotFound.jsx
+++ client/src/pages/404NotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound404() {
-  return <div>404NotFound</div>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "10vh" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/" style={{ color: "#007bff", textDecoration: "underline" }}>
+        Return to Home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound404;
